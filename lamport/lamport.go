171a16eef9f9a4ca363c1a0ba877116ee0436939
Kanerix <dkkasjoe@hotmail.com>
2024-11-11 19:24:19 +0100

    Lamport timestamp

---
+++ lamport/lamport.go
@@ -0,0 +1,29 @@
+package lamport
+
+import (
+	"sync/atomic"
+)
+
+type LamportClock struct {
+	time atomic.Uint64
+}
+
+func NewLamportClock() *LamportClock {
+	return &LamportClock{
+		time: atomic.Uint64{},
+	}
+}
+
+func (c *LamportClock) Now() uint64 {
+	return c.time.Load()
+}
+
+func (c *LamportClock) Step() {
+	c.time.Add(1)
+}
+
+func (c *LamportClock) Max(other uint64) {
+	if c.time.Load() < other {
+		c.time.Swap(other)
+	}
+}
